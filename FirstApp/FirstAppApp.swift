25082e0cebc809e10504950f47981145b28966a6
caio <caionogueirabindella@gmail.com>
2024-06-14 16:09:34 -0300

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Caio Bindella on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
