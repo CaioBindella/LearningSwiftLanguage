bc8b99894872a2223471edf1e8398452f57ba7b3
caio <caionogueirabindella@gmail.com>
2024-06-14 20:31:20 -0300

    My first app

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    
+    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+    
+    @State var selected = "Baseball"
+    @State private var id = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("\(selected)!")
+                .font(.title)
+            
+            VStack{
+                Circle()
+                    .fill(colors.randomElement() ?? .blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                            .font(.system(size: 120))
+                        )
+            }
+            .transition(.slide)
+            .id(id)
+            
+            Button("Next") {
+                withAnimation {
+                    selected = activities.randomElement() ?? "Archery"
+                    id += 1
+                    
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.system(size: 70))
         }
-        .padding()
+        
     }
 }
 
